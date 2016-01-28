ebeed9ddadddf130e5660ea9f5d1072eb1fe7a43
Sugarcoder <mikichea@gmail.com>
2016-01-27 22:06:40 -0800

    new commit for HW1

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+/**
+ * Created by Sugarcoder on 1/22/2016.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+
+
+}
---
+++ src/Sudoku.java
@@ -0,0 +1,16 @@
+/**
+ * Created by Sugarcoder on 1/27/2016.
+ */
+
+
+public class Sudoku {
+
+    public static void main(String[] args) {
+
+
+
+
+    }
+
+
+}
