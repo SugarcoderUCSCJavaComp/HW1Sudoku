434eebf515fcbd2d2fe0615196a424e90f1acfc6
Sugarcoder <mikichea@gmail.com>
2016-01-29 22:34:27 -0800

    progress

--- src/Sudoku.java
+++ src/Sudoku.java
@@ -1,14 +1,31 @@
-/**
- * Created by Sugarcoder on 1/27/2016.
- */
 
+import java.util.Scanner;
 
 public class Sudoku {
 
     public static void main(String[] args) {
 
+        System.out.println("What board size would you like to play on?\n"+
+                            "1) Size 4\n"+
+                            "2) Size 9");
 
+        Scanner scanner = new Scanner(System.in);
+        int boardSize = scanner.nextInt();
 
+        boolean quit = false;
+        while(!quit) {
+            switch (boardSize) {
+                case 1:
+                    System.out.println("This is the 4 x 4 board");
+                    board4by4();
+                    break;
+            }
+
+        }
+
+    }
+
+    public static void board4by4() {
 
     }
 
